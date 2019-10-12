14719e014496bf9b24aa7840524052df5b68b777
DanielRiloFerrero <40627401+DanielRiloFerrero@users.noreply.github.com>
2019-10-12 21:37:12 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: `XO Black Label`,
+    description: `A Clean, Minimal and Responsive Site`,
+    author: `XO Black`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-krisp`,
+        short_name: `krisp`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./content/data/`,
+      },
+    },
+  ],
+}
---
+++ src/subpages/LandingPage/LandingPage.js
@@ -0,0 +1,51 @@
+import React from "react"
+import Navbar from "../../components/Navbar"
+import { useStaticQuery, graphql } from "gatsby"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+import vidMP4 from '../../assets/videos/Wavez.mp4'
+import vidOGV from '../../assets/videos/Wavez.ogv'
+import vidWEBM from '../../assets/videos/Wavez.webm'
+
+
+
+import landingPageModule from './LandingPage.module.scss'
+import '../../assets/styles/modules/_glitch.scss'
+import '../../assets/styles/modules/_scroll.scss'
+
+function LandingPage() {
+  const { pageDataYaml } = useStaticQuery(graphql`
+    {
+      pageDataYaml {
+        welcomeText
+      }
+    }
+  `)
+
+  const { welcomeText } = pageDataYaml
+
+  return (
+    <section id="home" className={landingPageModule.wrapper}>
+      <div className={landingPageModule.videowrapper}>
+        <video autoPlay muted loop id="myVideo" className={landingPageModule.video}>
+          <source src={vidMP4} type="video/mp4"/>
+          <source src={vidOGV} type="video/ogv"/>
+          <source src={vidWEBM} type="video/webm"/>
+        </video>
+      </div>
+
+      <div className={landingPageModule.opac} />
+
+      <div className={landingPageModule.container}>
+        <Navbar />
+      </div>
+      <h1 className={`glitch ${landingPageModule.title}`} data-text="XO Blacc">{welcomeText}</h1>
+
+      <AnchorLink href='#services' className={landingPageModule.mouseblock}>
+        <div className="mouse" />
+      </AnchorLink>
+    </section>
+  )
+}
+
+export default LandingPage
